43198a22fb84bd77c53c6217c7d85fcf9b1e3a0c
jairo-sierra2117 <jairoenriquesb@ufps.edu.co>
2024-06-08 18:14:08 -0500

    test

---
+++ src/main/java/com/motorepuestos/melos/MelosApplication.java
@@ -0,0 +1,13 @@
+package com.motorepuestos.melos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MelosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MelosApplication.class, args);
+	}
+
+}
